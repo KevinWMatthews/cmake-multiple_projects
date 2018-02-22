0337110a6b5d73f250d3ec5a6072fd97adb16ef7
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-02-22 08:19:58 -0800

    Add library module

---
+++ library/module1.c
@@ -0,0 +1,7 @@
+#include "module1.h"
+#include <stdio.h>
+
+void module1_print(void)
+{
+    printf("This is module 1\n");
+}
---
+++ library/module1.h
@@ -0,0 +1,6 @@
+#ifndef MODULE1_INCLUDED
+#define MODULE1_INCLUDED
+
+void module1_print(void);
+
+#endif
